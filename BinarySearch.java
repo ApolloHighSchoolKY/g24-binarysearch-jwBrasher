3b5738349c97c819d039e560e9290d1efca61c64
jwBrasher <john.brasher@stu.daviess.kyschools.us>
2025-04-17 13:13:56 +0000

    Answers(do not grade)

--- BinarySearch.java
+++ BinarySearch.java
@@ -6,14 +6,23 @@ public class BinarySearch
 
 	public static int binarySearch(int[] ray, int item)
 	{
+
+		Arrays.sort(ray);
 		int bottom = ray[0];
-		int middle = (bottom + top)/2 ;
+		int middle = 0 ;
 		int top = ray.length-1;
 		
-		while()
-
-		if()
-
-
+		while(bottom<=top)
+		{
+			middle = (bottom + top)/2 ;
+		
+			if(ray[middle]<item)
+				bottom = middle + 1;
+			else if(ray[middle]>item)
+				top = middle - 1;
+			else
+				return middle;
+		}
+		return -1;
 	}
 }

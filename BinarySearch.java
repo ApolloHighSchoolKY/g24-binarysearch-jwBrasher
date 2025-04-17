67391e491dbd0813e72c25c225657153e60f52c0
jwBrasher <john.brasher@stu.daviess.kyschools.us>
2025-04-17 12:51:43 +0000

    A start

--- BinarySearch.java
+++ BinarySearch.java
@@ -2,8 +2,18 @@ import java.util.Arrays;
 
 public class BinarySearch
 {
+
+
 	public static int binarySearch(int[] ray, int item)
 	{
+		int bottom = ray[0];
+		int middle = (bottom + top)/2 ;
+		int top = ray.length-1;
+		
+		while()
+
+		if()
+
 
 	}
 }
